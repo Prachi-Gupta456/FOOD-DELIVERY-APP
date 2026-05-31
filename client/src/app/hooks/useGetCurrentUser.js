841576c0e245a2830633d73c167da7e40c2f0453
Prachi-Gupta456 <rg019436@gmail.com>
2026-05-31 13:14:30 +0530

    Modify useGetCurrentUser to accept skip parameter
    Add optional 'skip' parameter to useGetCurrentUser hook.

--- client/src/app/hooks/useGetCurrentUser.js
+++ client/src/app/hooks/useGetCurrentUser.js
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { setUserData } from "../redux/slices/userSlice"
 
-const useGetCurrentUser = () => {
+const useGetCurrentUser = (skip) => {
 
     const dispatch = useDispatch()
 
     useEffect(() => {
+
+           if(skip)return;
 
         const fetchUser = async () => {
             const result = await getCurrentUser()
