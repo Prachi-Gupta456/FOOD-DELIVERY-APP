f21faa61b74a9ee3bf21e9d99c8ea284708d559d
Prachi-Gupta456 <rg019436@gmail.com>
2026-05-31 13:14:58 +0530

    Update useGetItemByCity.js

--- client/src/app/hooks/useGetItemByCity.js
+++ client/src/app/hooks/useGetItemByCity.js
@@ -3,13 +3,14 @@ import { useDispatch, useSelector } from "react-redux"
 import { fetchItemsByCity } from "../services/api"
 import { setItemsInMyCity } from "../redux/slices/userSlice"
 
-const useGetItemByCity = () => {
+const useGetItemByCity = (skip) => {
     
     const { currentCity } = useSelector(state => state.user)
     const dispatch = useDispatch()
 
     useEffect(() => {
 
+           if(skip)return;
         const fetchItems = async () => {
             const result = await fetchItemsByCity(currentCity)
             if(result.items){
